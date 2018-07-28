f695c5c3c971b2f0fd2a8294d516bc835684a348
mike-del <delgado.portland@gmail.com>
2018-07-27 17:32:10 -0700

    Add accounts.login template and url

--- story_board/accounts/urls.py
+++ story_board/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from accounts.views import register
 
@@ -6,4 +7,5 @@ app_name = 'account'
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('login/', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
 ]
