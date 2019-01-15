ed41ec53e693bba784242d467be28d748ff486c2
dev-mike-del <delgado.portland@gmail.com>
2019-01-15 09:29:08 -0800

    Remove (to a non-source control file) and change SECRET_KEY

--- story_board/story_board/settings.py
+++ story_board/story_board/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import secret_key
+from . import secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_key.SECRET_KEY
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['story-board-dev.us-west-2.elasticbeanstalk.com',
                  '127.0.0.1',
